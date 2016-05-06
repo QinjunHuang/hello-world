6a03896a4bdbcec9a7791bcff70a3d1098b90923
tsun2003@163.com <kingtsun2003>
2016-05-06 11:51:37 +0800

    add word

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Response.Write("this is my first web to github!");
         Response.Write("hello world!");
     }
 }
